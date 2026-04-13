2fab71a5004d323860dbe0d195411744b3a93459
“bakosh2” <bakosh2@hotmail.com>
2026-04-13 10:30:20 +0000

    add 5-sum_list.py file

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module for summing a list of floats."""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Return the sum of all floats in input_list."""
+    return sum(input_list)
