6951f4e6677c23669bfeb97f19506899dfeec8b5
“bakosh2” <bakosh2@hotmail.com>
2026-04-13 10:07:29 +0000

    add 0-add.py file

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for adding two floating point numbers."""
+
+
+def add(a: float, b: float) -> float:
+    """Return the sum of two floats a and b."""
+    return a + b
