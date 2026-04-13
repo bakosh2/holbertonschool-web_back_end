9242bb4d7712b09fa11027a7208f0321ca080bf1
“bakosh2” <bakosh2@hotmail.com>
2026-04-13 11:13:54 +0000

    fix first file

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """Module for an async generator that yields random float numbers."""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Loop 10 times, wait 1 second each, then yield a random float 0-10."""
     for _ in range(10):
         await asyncio.sleep(1)
