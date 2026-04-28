b17db0bb303c27389d0a71fb15ffd7e04968a7fc
“bakosh2” <bakosh2@hotmail.com>
2026-04-28 14:38:06 +0000

    add file 12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,27 +1,25 @@
 #!/usr/bin/env python3
-"""provides stats about nginx logs stored in MongoDB"""
-
+'''
+Provide stats about Nginx logs stored in MongoDB
+'''
 from pymongo import MongoClient
 
+def log_stats():
+    x = MongoClient('mongodb://localhost:27017').logs.nginx
+
+
+    print(f'{x.count_documents({})} logs')
+
+ 
+    print('Methods:')
+    print(f'\tmethod GET: {x.count_documents({"method": "GET"})}')
+    print(f'\tmethod POST: {x.count_documents({"method": "POST"})}')
+    print(f'\tmethod PUT: {x.count_documents({"method": "PUT"})}')
+    print(f'\tmethod PATCH: {x.count_documents({"method": "PATCH"})}')
+    print(f'\tmethod DELETE: {x.count_documents({"method": "DELETE"})}')
+
+
+    print(f'{x.count_documents({"method": "GET", "path": "/status"})} status check')
 
 if __name__ == "__main__":
-    client = MongoClient("mongodb://127.0.0.1:27017")
-    collection = client.logs.nginx
-
-    print("{} logs".format(collection.count_documents({})))
-    print("Methods:")
-
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-    for m in methods:
-        print("    method {}: {}".format(
-            m,
-            collection.count_documents({"method": m})
-        ))
-
-    print("{} status check".format(
-        collection.count_documents({
-            "method": "GET",
-            "path": "/status"
-        })
-    ))
+    log_stats()
