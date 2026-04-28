132bf203a609aa4ff547a73dbe6ceedb6576006d
“bakosh2” <bakosh2@hotmail.com>
2026-04-28 14:06:56 +0000

    fix log stats formatting

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -5,21 +5,20 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
+    client = MongoClient("mongodb://127.0.0.1:27017")
     collection = client.logs.nginx
 
-    # total logs
     print(f"{collection.count_documents({})} logs")
-
     print("Methods:")
 
-    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    for m in methods:
         print("\tmethod {}: {}".format(
-            method,
-            collection.count_documents({"method": method})
+            m,
+            collection.count_documents({"method": m})
         ))
 
-    # status check
     print("{} status check".format(
         collection.count_documents({
             "method": "GET",
