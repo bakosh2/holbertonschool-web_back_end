e4bb9836cac73ec523634488a8b8636367317011
“bakosh2” <bakosh2@hotmail.com>
2026-05-09 19:30:40 +0000

    fix task 1 final

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,9 +3,7 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
-    [task, task2];
+    console.log(task, task2);
   }
 
   return [task, task2];
