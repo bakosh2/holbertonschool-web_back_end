53a2125a04af4a5296da8db063ad90fd4dfd94f0
bakosh2 <bakosh2@github.com>
2026-05-18 19:27:35 +0000

    feat(Node_JS_basic): task 1 - read user input from stdin

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+// Listen for data from standard input
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+
+// Listen for the end of the input stream
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
